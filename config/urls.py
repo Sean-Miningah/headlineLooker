7bc7f652f3f5482dd03b1472757e63590d629be9
Sean-Miningah <seanpminingah@gmail.com>
2023-06-04 00:28:21 +0300

    Updated Health View

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,10 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
+from .healthview import healthy_view
+
 urlpatterns = [
+    path('healthz', healthy_view),
     path('admin/', admin.site.urls),
     # path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
