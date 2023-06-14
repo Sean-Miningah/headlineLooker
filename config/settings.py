74520cff1efea5066cfa20eb3588ebb655001342
Sean-Miningah <miningahsean@gmail.com>
2023-06-14 20:40:29 +0300

    Rabbitmq setup

--- config/settings.py
+++ config/settings.py
@@ -184,10 +184,8 @@ CLOUD_REDIS = os.getenv('CLOUD_REDIS') in ('True',)
 
 if CLOUD_REDIS:
     CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
-    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
 else:
     CELERY_BROKER_URL='redis://localhost:6379'
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
 
 
 CELERY_TIMEZONE = 'Africa/Nairobi'
