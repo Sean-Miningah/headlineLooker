c03b4ec87741616d8b5e110f56f9bf41d534a0b0
Sean-Miningah <miningahsean@gmail.com>
2023-06-15 09:04:57 +0300

    ft: Integrating APScheduler for tasks management

--- scraper/apps.py
+++ scraper/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ScraperConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scraper'
+    
+    def ready(self):
+        from scraper.update import jumia_scrape
+        jumia_scrape()
--- scraper/update.py
+++ scraper/update.py
@@ -11,20 +11,20 @@ smartphone_url = os.getenv('JUMIA_SMARTPHONE_URL')
 
 
 def save_jumia():
-  try:
-    jumia = JumiaScraper(smartphone_url)
-    jumia_data = jumia.scrape_data()
-    for data in jumia_data:
-      product = Product(
-        **data
-      )
-      product.save()  
-  except Exception as e:
-    print(e)
-    
+	try:
+		jumia = JumiaScraper(smartphone_url)
+		jumia_data = jumia.scrape_data()
+		for data in jumia_data:
+			product = Product(
+			**data
+			)
+			product.save()  
+	except Exception as e:
+		print(e)
+	
 
 def jumia_scrape():
-  scheduler = BackgroundScheduler()
-  scheduler.add_job(save_jumia, 'cron', hour='8, 13', minute='0', timezone=nairobi)
-  scheduler.start()
+	scheduler = BackgroundScheduler()
+	scheduler.add_job(save_jumia, 'cron', hour='9, 13', minute='0', timezone=nairobi)
+	scheduler.start()
 
